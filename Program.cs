b2fbbea9e63794b9e77297a9b81cc54edb5e3a64
Choppa <slamrotlomal@gmail.com>
2024-06-19 14:13:51 +0300

    tutu
    1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nitent_forms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.WriteLine("Введите значение: x");
+            x = double.Parse(Console.ReadLine());
+            y = 4 * Math.Sin(x) * (5 * Math.Cos(x));
+            Console.WriteLine("y = " + Math.Round(y, 2));
+            Console.ReadLine();
+        }
+    }
+}
